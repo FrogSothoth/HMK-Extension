78c9fecc8573eacde2a89ab8b3963fd0cbb679b3
FrogSothoth <dmmilner@gmail.com>
2026-03-11 20:36:23 -0500

    Finished redo of Gear tab

--- extension/scripts/tab_gear.lua
+++ extension/scripts/tab_gear.lua
@@ -22,3 +22,70 @@ function onDataUpdate()
         ArmorManager.calculateGear(node);
     end
 end
+
+function onDrop(x, y, draginfo)
+	if draginfo.isType("shortcut") then
+		local class, datasource = draginfo.getShortcutData();
+		if class == "item" then
+			local nodeItem = DB.findNode(datasource);
+			if nodeItem then
+				addItem(nodeItem);
+			end
+			return true;
+		end
+	end
+end
+
+function addItem(nodeItem)
+	local sName = DB.getValue(nodeItem, "name", "");
+	local nWeight = DB.getValue(nodeItem, "weight", 0);
+	
+	-- Get category from multiple possible sources
+	local sCategory = DB.getValue(nodeItem, "category", "");
+	if sCategory == "" then
+		sCategory = DB.getCategory(DB.getPath(nodeItem)) or "";
+	end
+	
+	-- Trim and sanitize category search
+	local sCategorySearch = sCategory:gsub("^%s*(.-)%s*$", "%1"):upper();
+	
+	-- Check if it is Armour/Clothing
+	local bIsArmour = (sCategorySearch == "ARMOUR/CLOTHING");
+	
+	-- Fallback to ArmorData lookup if DB category didn't catch it
+	if not bIsArmour then
+		if ArmorData and ArmorData.lookupItem then
+			if ArmorData.lookupItem(sName) then
+				bIsArmour = true;
+			end
+		end
+	end
+
+	local nodeChar = getDatabaseNode();
+	if not nodeChar then return; end
+
+	if bIsArmour then
+		local nodeEntry = DB.createChild(DB.createChild(nodeChar, "armourlist"));
+		DB.setValue(nodeEntry, "name", "string", sName);
+		DB.setValue(nodeEntry, "weight", "number", nWeight);
+	else
+		-- Check if item already exists in possessions
+		local bFound = false;
+		local nodePossessions = DB.createChild(nodeChar, "possessions");
+		for _,v in pairs(DB.getChildren(nodePossessions)) do
+			if DB.getValue(v, "name", "") == sName then
+				local nQty = DB.getValue(v, "quantity", 1);
+				DB.setValue(v, "quantity", "number", nQty + 1);
+				bFound = true;
+				break;
+			end
+		end
+
+		if not bFound then
+			local nodeEntry = DB.createChild(nodePossessions);
+			DB.setValue(nodeEntry, "name", "string", sName);
+			DB.setValue(nodeEntry, "weight", "number", nWeight);
+			DB.setValue(nodeEntry, "quantity", "number", 1);
+		end
+	end
+end
